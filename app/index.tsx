67dc390e332ac2b478f7abb9ba27cc4199e4585a
MiniLancelot <nhattnm.ilt@gmail.com>
2024-11-12 16:06:58 +0700

    adjust map diretion api request

--- app/index.tsx
+++ app/index.tsx
@@ -34,7 +34,7 @@ const Index = () => {
         if (session) {
             router.replace("/home");
         } else {
-            router.replace("/profile");
+            router.replace("/onboarding");
         }
     }
     return (
